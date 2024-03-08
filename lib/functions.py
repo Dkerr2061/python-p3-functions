a72ca4827dd8e30c0c529b5b5f121c9d9af60b9a
David Kerr <kerrky@gmail.com>
2024-03-08 12:56:01 -0500

    complete lab

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,21 @@
 #!/usr/bin/env python3
 
+name= 'David'
+num1= 2
+num2= 4
+number= 10
+
 def greet_programmer():
-    pass
+    print('Hello, programmer!')
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
 
 def add(num1, num2):
-    pass
+    return num1 + num2
 
 def halve(number):
-    pass
+    return number / 2
